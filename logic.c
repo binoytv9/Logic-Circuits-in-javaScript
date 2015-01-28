edb75806ce7753872d3fa9b90e7e55d07fa2f2c7
binoytv9 <binoytv9@gmail.com>
2015-01-28 18:51:05 +0530

    interconnection working

--- logic.c
+++ logic.c
@@ -20,40 +20,42 @@ struct wire{
 
 struct gate{
 	char *name;
-	struct wire *A,*B,*C;
+	void (*evaluate)(struct gate *);
 	struct NOT *not;
-	struct AND *and;
+	struct GATE2 *gate2;
 	struct XOR *xor;
 	struct HA *ha;
 	struct FA *fa;
-	void (*evaluate)(struct gate *);
 };
 	
 struct NOT{
-	struct wire *A;
-	struct wire *B;
+	struct wire *A;		// in
+	struct wire *B;		// out
 };
 	
-struct AND{
-	struct wire *A,*B;
-	struct wire *C;
+struct GATE2{
+	struct wire *A,*B;	// in
+	struct wire *C;		// out
 };
 
 struct XOR{
+	struct wire *A,*B;	// in
+	struct wire *C;		// out
 	struct AND *A1,*A2;
 	struct NOT *I1,*I2;
 	struct OR *O1;
 };
 
 struct HA{
-	struct wire *A,*B;
-	struct wire *S,*C;
+	struct wire *A,*B;	// in
+	struct wire *S,*C;	// out
 	struct XOR *X1;
 	struct AND *A1;
 };
 
 struct FA{
-	struct wire *A,*B,*Cin,*S,*Cout;
+	struct wire *A,*B,*Cin;	// in
+	struct wire *S,*Cout;	// out
 	struct HA *H1,*H2;
 	struct OR *O1;
 };
@@ -65,35 +67,46 @@ void eval_or(struct gate *this);
 void eval_and(struct gate *this);
 void eval_not(struct gate *this);
 void eval_default(struct gate *this);
-void LC(struct gate *this, char *name);
-void Gate2(struct gate *this, char *name);
+void LC(struct gate **thisref, char *name);
+void Gate2(struct gate **this, char *name);
 void setMethod(struct wire *this, int value);
 void connectMethod(struct wire *this, int count, ...);
 struct wire *Connector(struct gate *owner, char *name, int activates, int monitor);
 
 main()
 {
-	struct gate *n = Not("N1");
-	struct gate *a = And("A1");
-	struct gate *o = Or("O1");
-
+/*
 	printf("\nNot output\n");
-	n->B->monitor = 1;
-	n->A->set(n->A,0);
-	n->A->set(n->A,1);
+	struct gate *n = Not("N1");
+	n->not->B->monitor = 1;
+	n->not->A->set(n->not->A,0);
+	n->not->A->set(n->not->A,1);
 	printf("\n\n");
 
 	printf("\nAnd output\n");
-	a->C->monitor = 1;
-	a->A->set(a->A,1);
-	a->B->set(a->B,1);
+	struct gate *a = And("A1");
+	a->gate2->C->monitor = 1;
+	a->gate2->A->set(a->gate2->A,1);
+	a->gate2->B->set(a->gate2->B,1);
 	printf("\n\n");
 
 	printf("\nOr output\n");
-	o->C->monitor = 1;
-	o->A->set(o->A,0);
-	o->B->set(o->B,1);
+	struct gate *o = Or("O1");
+	o->gate2->C->monitor = 1;
+	o->gate2->A->set(o->gate2->A,0);
+	o->gate2->B->set(o->gate2->B,1);
 	printf("\n\n");
+
+	printf("\nNand output : connecting AND to NOT\n");
+	struct gate *a = And("A1");
+	struct gate *n = Not("N1");
+	a->gate2->C->monitor = 1;
+	a->gate2->C->connect(a->gate2->C,1,n->not->A);
+	n->not->B->monitor = 1;
+	a->gate2->B->set(a->gate2->B,1);
+	a->gate2->A->set(a->gate2->A,1);
+	printf("\n\n");
+*/
 }
 
 struct wire *Connector(struct gate *owner, char *name, int activates, int monitor)
@@ -130,7 +143,7 @@ void connectMethod(struct wire *this, int count, ...)
 	va_start(ap,count);
 	for(i = 0; i < count; i++)
 		(this->connects)[i] = va_arg(ap, struct wire *);
-	(this->connects)[i] = NULL;
+	(this->connects)[i] = NULL; /* setting last element to NULL */
 
 	va_end(ap);
 }
@@ -152,14 +165,18 @@ void setMethod(struct wire *this, int value)
 		(this->connects)[i]->set((this->connects)[i], value);
 }
 
-void LC(struct gate *this, char *name)
+void LC(struct gate **thisref, char *name)
 {
-	if((this->name = strdup(name)) == NULL){
+	if((*thisref = (struct gate *)malloc(sizeof(struct gate))) == NULL){
+		fprintf(stderr,"\nerror : unable to allocate memory for instance\n");
+		exit(1);
+	}
+	if(((*thisref)->name = strdup(name)) == NULL){
 		fprintf(stderr,"\nerror : unable to allocate memory for name\n");
 		exit(2);
 	}
 
-	this->evaluate = eval_default;
+	(*thisref)->evaluate = eval_default;
 }
 
 void eval_default(struct gate *this)
@@ -171,13 +188,13 @@ struct gate *Not(char *name)
 {
 	struct gate *this = NULL;
 
-	if((this = (struct gate *)malloc(sizeof(struct gate))) == NULL){
-		fprintf(stderr,"\nerror : unable to allocate memory for instance\n");
-		exit(1);
+	LC(&this, name);
+	if((this->not = (struct NOT *)malloc(sizeof(struct NOT))) == NULL){
+		fprintf(stderr,"\nerror : unable to allocate memory for gate->NOT\n");
+		exit(3);
 	}
-	LC(this, name);
-	this->A = Connector(this,"A",1,0);
-	this->B = Connector(this,"B",0,0);
+	this->not->A = Connector(this,"A",1,0);
+	this->not->B = Connector(this,"B",0,0);
 	this->evaluate = eval_not;
 
 	return this;
@@ -185,47 +202,46 @@ struct gate *Not(char *name)
 
 void eval_not(struct gate *this)
 {
-	this->B->set(this->B, (this->A->value == 1) ? 0 : 1);
+	this->not->B->set(this->not->B, (this->not->A->value == 1) ? 0 : 1);
 }
 
-void Gate2(struct gate *this, char *name)
+void Gate2(struct gate **thisref, char *name)
 {
-	LC(this, name);
-	this->A = Connector(this,"A",1,0);
-	this->B = Connector(this,"B",1,0);
-	this->C = Connector(this,"C",0,0);
+	LC(thisref, name);
+	if(((*thisref)->gate2 = (struct GATE2 *)malloc(sizeof(struct GATE2))) == NULL){
+		fprintf(stderr,"\nerror : unable to allocate memory for instance\n");
+		exit(3);
+	}
+
+	struct gate *this = *thisref;
+
+	this->gate2->A = Connector(this,"A",1,0);
+	this->gate2->B = Connector(this,"B",1,0);
+	this->gate2->C = Connector(this,"C",0,0);
 }
 
 struct gate *And(char *name)
 {
 	struct gate *this = NULL;
 
-	if((this = (struct gate *)malloc(sizeof(struct gate))) == NULL){
-		fprintf(stderr,"\nerror : unable to allocate memory for instance\n");
-		exit(1);
-	}
-	Gate2(this,name);
+	Gate2(&this,name);
 	this->evaluate = eval_and;
 }
 
 void eval_and(struct gate *this)
 {
-	this->C->set(this->C, (this->A->value == 1) && (this->B->value == 1));
+	this->gate2->C->set(this->gate2->C, (this->gate2->A->value == 1) && (this->gate2->B->value == 1));
 }
 
 struct gate *Or(char *name)
 {
 	struct gate *this = NULL;
 
-	if((this = (struct gate *)malloc(sizeof(struct gate))) == NULL){
-		fprintf(stderr,"\nerror : unable to allocate memory for instance\n");
-		exit(1);
-	}
-	Gate2(this,name);
+	Gate2(&this,name);
 	this->evaluate = eval_or;
 }
 
 void eval_or(struct gate *this)
 {
-	this->C->set(this->C, (this->A->value == 1) || (this->B->value == 1));
+	this->gate2->C->set(this->gate2->C, (this->gate2->A->value == 1) || (this->gate2->B->value == 1));
 }
